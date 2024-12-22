de1d13b3d91b71dfef356bc7ce40384cefd836a9
k.revyakina <k.revyakina@kata.academy>
2024-12-22 12:48:07 +0300

    fix: изменение min-width на max-width в Layout

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ const Layout: FC<PropsWithChildren<{ sidebar?: boolean }>> = ({
   children,
 }) => {
   return (
-    <div className="flex flex-col grow 2xl:min-w-[1440px] xl:min-w-[1024px]">
+    <div className="flex flex-col grow 2xl:max-w-[1440px] xl:max-w-[1024px]">
       <div className="flex flex-1">
         {sidebar && <Sidebar />}
         <main className="grow p-6">{children}</main>
